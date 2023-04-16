54d1c32aea340d9cb0c246615a9462333a27c6af
Хасан Хамраев <khasanknyazev81@gmail.com>
2023-04-16 11:56:28 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# токен для доступа к chatgpt
+ai_token = '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'
+
+# токен для доступа к телеграм боту
+tele_token = '5742822138:AAGy9sWEnwlgFazJveISC7XxJP5BdtmRifg'
+
+# вводные ключи доступа к боту
+key_for_bot = ['tok3c24a3d2b42add132', 'tokd432dbaacb4c2bb4b',
+               'tokcd22bbca4dd3cd44c', 'tok14dd3133422c3121b'
+               ]
+
+admin_pass = 'AAGy9sWEnwlgFazJveISC7XxJP5BdtmRifg'
+
+my_id = '782697565'
