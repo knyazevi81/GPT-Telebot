4e9b99e0de98971d58575b707ca81391eb70b117
Хасан Хамраев <khasanknyazev81@gmail.com>
2023-04-17 01:42:11 +0300

    Update config,py

--- config.py
+++ config.py
@@ -1,13 +1,17 @@
 # токен для доступа к chatgpt
-ai_token = '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'
+ai_token = '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'
 
 # токен для доступа к телеграм боту
 tele_token = '5742822138:AAGy9sWEnwlgFazJveISC7XxJP5BdtmRifg'
 
 # вводные ключи доступа к боту
-key_for_bot = ['tok3c24a3d2b42add132', 'tokd432dbaacb4c2bb4b',
-               'tokcd22bbca4dd3cd44c', 'tok14dd3133422c3121b'
-               ]
+key_for_bot = ['tok3c24a3d2b42add132', 'tokd432dbaacb4c2bb4b', 'tokcd22bbca4dd3cd44c',
+               'tok14dd3133422c3121b', 'tok3c34a3a2b40add122', 'tok8c34a3n2b40khd142',
+               'tok8334akhabs0khd142']
+
+test_users = [782697565, 1547842533, 1164415629,
+              603861098, 1922539111, 403505597,
+              903254797, 857477574]
 
 admin_pass = 'AAGy9sWEnwlgFazJveISC7XxJP5BdtmRifg'
 
