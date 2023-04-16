5c918d9e1e03c4b0205dd7a5c486287c5b996cbf
Khasan Khamraev <73337376+knyazevi81@users.noreply.github.com>
2023-04-17 01:59:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,17 @@
 # токен для доступа к chatgpt
-ai_token = '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'
+ai_token = 'sk-absdrsdT1bTqJ8FT3BlbkFJkg8QLKhblQK3oJwSef7t'
 
 # токен для доступа к телеграм боту
-tele_token = '5742822138:AAGy9sWEnwlgFazJveISC7XxJP5BdtmRifg'
+tele_token = '5742822138:AbcdrdtEnwlgFazJveISC7XxJP5BdtmRifg'
 
-# вводные ключи доступа к боту
-key_for_bot = ['tok3c24a3d2b42add132', 'tokd432dbaacb4c2bb4b', 'tokcd22bbca4dd3cd44c',
-               'tok14dd3133422c3121b', 'tok3c34a3a2b40add122', 'tok8c34a3n2b40khd142',
-               'tok8334akhabs0khd142']
+# вводные ключи доступа к боту (они сразу добавляются в бд) list[str]
+key_for_bot = [ ]
 
+# вводные зарегистрированные айди для особой рассылки list[int]
 test_users = [782697565, 1547842533, 1164415629,
               603861098, 1922539111, 403505597,
               903254797, 857477574]
 
-admin_pass = 'AAGy9sWEnwlgFazJveISC7XxJP5BdtmRifg'
+# ключ доступа к админ панели
+admin_pass = 'AAGy9sWEnJveISCdtmRifg'
 
-my_id = '782697565'
